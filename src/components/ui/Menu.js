4ab3eb88683ed500278abb02f7fccbfb91e169b6
Kadir Demirel <Kadirdemirel_17@hotmail.com>
2022-01-24 14:45:27 +0300

    Update

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import { Collapse } from "react-collapse";
 import { useWindowWidth } from "@react-hook/window-size";
